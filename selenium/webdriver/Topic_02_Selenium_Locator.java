5690222d2525aa13ef22c7f7dd764b3ade2255b9
hainn <hai.nn191297@gmail.com>
2024-01-08 16:41:26 +0700

    update Topic_O2

--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
@@ -23,21 +24,64 @@ public class Topic_02_Selenium_Locator {
 
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/register");
+       // driver.manage().window().maximize();
+    }
+
+    //Test NG: order testcase theo Alphabet (0-9 A-Z)
+    @Test
+    public void TC_01_ID() {
+       driver.findElement(By.id("FirstName")).sendKeys("Keane");
     }
 
     @Test
-    public void TC_01_() {
+    public void TC_02_Class() {
+        driver.findElement(By.className("header-logo"));
 
+    }
+    @Test
+    public void TC_03_Name() {
+        driver.findElement(By.name("DateOfBirthDay"));
     }
 
     @Test
-    public void TC_02_() {
+    public void TC_04_TagName() {
+        driver.findElements(By.tagName("input"));
+    }
+    @Test
+    public void TC_05_LinkText() {
+        driver.findElements(By.linkText("Shipping & returns"));
 
     }
+    @Test
+    public void TC_06_Partial_LinkText() {
+        driver.findElement(By.partialLinkText("vendor account"));
+    }
+
+    @Test
+    public void TC_07_Css() {
+        //Css with Id
+        driver.findElements(By.cssSelector("input[id='FirstName']"));
+        driver.findElements(By.cssSelector("input#FirstName"));
+        driver.findElements(By.cssSelector("#FirstName"));
+
+        //Css with Class
+        driver.findElements(By.cssSelector("div[class='page-title']"));
+        driver.findElements(By.cssSelector("div.page-title"));
+        driver.findElements(By.cssSelector(".page-title"));
+
+        //Css with Name
+        driver.findElements(By.cssSelector("input[name='FirstName']"));
+
+        //Css with tagName
+        driver.findElement((By.cssSelector("input")));
+
+    }
+
+
 
     @AfterClass
     public void afterClass() {
-        driver.quit();
+       // driver.quit();
     }
 }
